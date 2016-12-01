e426db2f83f2d06107450e3f1b79e4af40b4d98e
Cole Deisseroth <karl@dn51u41j.sunet>
2016-12-01 13:03:27 -0800

    Added people to the join-project screen
    Also, I temporarily disabled the authentication. Now, no matter what is
    in the text fields, your login is accepted. This was mostly for
    convenience until we've gotten the customizable login system fully
    operational.

--- app/src/main/java/hifive/hifive/JoinProject.java
+++ app/src/main/java/hifive/hifive/JoinProject.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class JoinProject extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class JoinProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_project);
+        TextView projectTitle = (TextView) findViewById(R.id.project_title_jp);
+        projectTitle.setText(((MyApplication)this.getApplication()).getActiveProjectTitle());
     }
     public void goToMain(View view){
         Intent intent = new Intent(this, MainActivity.class);
